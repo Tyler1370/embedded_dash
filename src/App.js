77687a4fb7b76ba2b3c94c21a0b73b669a4f6849
Tyler Nguyen <tylerkn1@uci.edu>
2024-02-13 00:58:58 -0800

    mph needle

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ function App() {
   return (
     <div className="flex flex-col justify-between items-center text-center w-[800px] h-[480px] p-10 bg-black text-white ">
       {/* header */}
-      <div className="flex flex-row justify-between items-center w-full px-[150px]">
+      <div className="flex flex-row justify-between items-center w-fit gap-8">
         <div className="flex flex-row justify-center items-center gap-2">
           <p className="">IMD</p>
-          <div className="w-3 h-3 rounded-full bg-white"></div>
+          <div className="w-2 h-2 rounded-full bg-white"></div>
         </div>
         <div className="flex flex-row justify-center items-center gap-2">
           <p className="">PCC</p>
-          <div className="w-3 h-3 rounded-full bg-white"></div>
+          <div className="w-2 h-2 rounded-full bg-white"></div>
         </div>
         <div>
           <p className="">60.4 A</p>
@@ -26,18 +26,21 @@ function App() {
       {/* speed */}
       <div className="relative flex flex-col gap-5 border-solid border-2 rounded-full w-60 h-60 justify-center items-center">
         {/* <div className="absolute"> */}
-        <p className="absolute left-2 text-xs text-center">0</p>
-        <p className="absolute left-[34px] top-[44px] text-xs text-center">
+        <p className="absolute left-2 text-xs text-center z-10">0</p>
+        <p className="absolute left-[34px] top-[44px] text-xs text-center z-10">
           20
         </p>
-        <p className="absolute left-[108px] top-[6px] text-xs text-center">
+        <p className="absolute left-[108px] top-[6px] text-xs text-center z-10">
           40
         </p>
-        <p className="absolute right-[34px] top-[44px] text-xs text-center">
+        <p className="absolute right-[34px] top-[44px] text-xs text-center z-10">
           60
         </p>
-        <p className="absolute right-2 text-xs text-center">80</p>
-        {/* </div> */}
+        <p className="absolute right-2 text-xs text-center z-10">80</p>
+        {/* <div className="absolute left-0 bg-green-200 w-1 h-1 border-l-1 border-solid border-transparent"></div> */}
+        <div className="w-full h-full">
+          <div className="needle z-0 drop-shadow-[0_0px_4px_#00FFF7]"></div>
+        </div>
         <div className="absolute flex flex-row items-end">
           <p className="text-6xl font-bold">17</p>
           <p className="text-base -ml-1">mph</p>
@@ -46,9 +49,9 @@ function App() {
       {/* footer */}
       <div className="flex flex-col justify-between w-fit">
         {/* energy bar */}
-        <div className="w-80 h-1 bg-cyan-400 bg-opacity-30 ">
+        <div className="w-80 h-1 bg-[#00FFF7] bg-opacity-20 ">
           <div
-            className="h-1 bg-cyan-400 shadow-2xl shadow-cyan-400"
+            className="h-1 bg-[#00FFF7] drop-shadow-[0_0px_8px_#00FFF7] shadow-cyan-400"
             style={{ width: `${(energy / 100) * 320}px` }}
           ></div>
         </div>
